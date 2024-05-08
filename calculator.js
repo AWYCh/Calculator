954dd9dae0ab4f34482e36d023347b51226ecf28
AWYCh <AWYCh@users.noreply.github.com>
2024-05-08 18:41:02 +0800

    Create buttons and add some CSS styling

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
+const numbers = document.querySelectorAll(".numberpad");
+const operators = document.querySelectorAll(".operators");
+const display = document.querySelector("#display");
+const clear = document.querySelector("#clear");
+
+display.textContent = 0;
+
+const firstNumber = function getFirstNumber() {
+
+};
+
+const secondNumber = function getSecondNumber() {
+
+};
+
+const operator = function getOperator() {
+
+};
+
 const add = function(a,b) {
     return a + b;
 };
@@ -13,3 +32,17 @@ const multiply = function(a,b) {
 const divide = function (a,b) {
     return a/b;
 };
+
+function getNumber(e) {
+    display.textContent = e.target.id;
+}
+
+function clearDisplay() {
+    display.textContent = 0;
+};
+
+numbers.forEach(button => {
+    button.addEventListener("click", getNumber);
+});
+
+clear.addEventListener("click", clearDisplay);
