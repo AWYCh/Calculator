82a2fbaa1657928bed8c1cd02975780398838f4d
AWYCh <AWYCh@users.noreply.github.com>
2024-05-14 18:38:02 +0800

    Add function that allows keyboard input

--- calculator.js
+++ calculator.js
@@ -1,10 +1,11 @@
 const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operators");
+const operators = document.querySelectorAll(".operator");
 const display = document.querySelector("#display");
 const stored = document.querySelector("#stored");
 const clear = document.querySelector("#clear");
 const equals = document.querySelector("#equals");
 const decimal = document.querySelector("#decimal");
+const backspace = document.querySelector("#backspace");
 
 let currentNumber = "";
 let previousNumber = "";
@@ -34,16 +35,17 @@ function operate (a,b) {
     a = Number(a);
     b = Number(b);
 
-    if (operator === "add") {
+    if (operator === "+") {
         currentNumber = add(a,b);
-    } else if (operator === "subtract") {
+    } else if (operator === "-") {
         currentNumber = subtract(a,b);
-    } else if (operator === "multiply") {
+    } else if (operator === "x") {
         currentNumber = multiply(a,b);
-    } else if (operator === "divide") {
+    } else if (operator === "/") {
         if (b === 0) {
             currentNumber = "MissingNo";
             display.textContent = currentNumber;
+            operator = "";
             return;
         } else {
             currentNumber = divide(a,b);
@@ -53,25 +55,26 @@ function operate (a,b) {
     stored.textContent += " " + b + " " + "=";
 };
 
-function getNumber(e) {
+function getNumber(number) {
     if (operator == "" && currentNumber !== "" && previousNumber !== "") {
         clearDisplay()
     };
     if (currentNumber.length <= 15) {
-    currentNumber += e.target.textContent;
+    currentNumber += number;
     display.textContent = currentNumber;
-    }};
+    }
+};
 
-function getOperator(e) {
+function getOperator(op) {
     if (currentNumber == "") {
         currentNumber = 0
     } else if (operator !== "") {
         operate(previousNumber, currentNumber)
     };
-    operator = e.target.id;
+    operator = op;
     previousNumber = currentNumber;
     currentNumber = "";
-    stored.textContent = previousNumber + " " + e.target.textContent;
+    stored.textContent = previousNumber + " " + operator;
 };
 
 function getDecimal () {
@@ -81,7 +84,15 @@ function getDecimal () {
         };
         currentNumber += ".";
         display.textContent = currentNumber;
-    }};
+    }
+};
+
+function undo() {
+    if (currentNumber !== "") {
+        currentNumber = currentNumber.slice(0,-1);
+        display.textContent = currentNumber;
+    }
+};
 
 function clearDisplay() {
     currentNumber = "";
@@ -91,18 +102,44 @@ function clearDisplay() {
     stored.textContent = "";
 };
 
+function getKey(e) {
+    e.preventDefault();
+    if (e.key >= 0 && e.key <= 9) {
+        getNumber(e.key);
+    } else if (e.key === "+" || e.key === "-" || e.key === "/") {
+        getOperator(e.key);
+    } else if (e.key === "*") {
+        getOperator("x");
+    } else if ((e.key === "=" && currentNumber !== "" && previousNumber !== "" && operator !== "")|| 
+                (e.key === "Enter" && currentNumber !== "" && previousNumber !== "" && operator !== "")) {
+        operate(previousNumber, currentNumber);
+    } else if (e.key === ".") {
+        getDecimal();
+    } else if (e.key === "Backspace") {
+        undo();
+    } else if (e.key === "Escape") {
+        clearDisplay();
+    }
+};
+
 numbers.forEach(button => {
-    button.addEventListener("click", getNumber);
+    button.addEventListener("click", (e) => {
+        getNumber(e.target.textContent)});
 });
 
 operators.forEach(button => {
-    button.addEventListener("click", getOperator);
+    button.addEventListener("click", (e) => {
+        getOperator(e.target.textContent)});
 });
 
 decimal.addEventListener("click", getDecimal);
 
-equals.addEventListener("click", function () {
+equals.addEventListener("click", () => {
     if (currentNumber !== "" && previousNumber !== "" && operator !== "") {
     operate(previousNumber, currentNumber)}});
 
+backspace.addEventListener("click", undo);
+
 clear.addEventListener("click", clearDisplay);
+
+document.addEventListener("keydown", getKey);
